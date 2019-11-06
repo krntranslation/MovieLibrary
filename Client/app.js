05b3332d12889cf07c2dd60a9e21093c11255934
unknown <krntranslation@gmail.com>
2019-11-06 07:58:15 -0600

    first commit for wednesday, still working on the get method for jquery

--- Client/app.js
+++ Client/app.js
@@ -24,4 +24,36 @@
     }
 
     $('#my-form').submit(processForm);
+    
+})(jQuery);
+(function($){
+    function getRequest( e ){
+        var dict = {
+        	Title : this["title"].value,
+            DirectorName: this["directorName"].value,
+            Genre: this["genre"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44352/api/movie',
+            dataType: 'json',
+            type: 'get',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').append($("<tr")
+                .append($("<td>").append(this.title))
+                .append($("<td>").append(this.directorName))
+                .append($("<td>").append(this.genre)))
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#my-get').submit(getRequest);
+    
 })(jQuery);
